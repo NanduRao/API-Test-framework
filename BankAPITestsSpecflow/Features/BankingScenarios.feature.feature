7fe8dc781373256780911eacb1889d09d2999b1c
NanduRao <nandu.crao@gmail.com>
2024-02-03 19:41:26 +0530

    Initial Commit

---
+++ BankAPITestsSpecflow/Features/BankingScenarios.feature.feature
@@ -0,0 +1,39 @@
+﻿Feature: Banking Operations
+
+  Scenario Outline: A user can create an account with different initial balances 
+    Given the user sends the request to create account with details '<InitialBalance>','<AccountName>', and '<Address>'
+    Then I verify the status code '<statusCode>' 
+    And verify the response data '<InitialBalance>','<NewBalance>','<AccountName>','<AccountNumber>' and '<Message>'
+
+    Examples:
+      |statusCode| InitialBalance | AccountName     | Address           | NewBalance | AccountNumber | Message                              |
+      |  200     | 1000           | Rajesh Mittal   | Ahmedabad, Gujarat| 1000       | X123            | Account X123 created successfully   |
+      |  200     | 2000           | John Doe         | New York, USA     | 2000       | Y456            | Account Y456 created successfully   |
+
+Scenario Outline: A user can delete an account with different account IDs
+    Given the user has an account with AccountID '<AccountID>'
+    And verify the response data '<Data>' and '<Message>'
+      
+    Examples:
+      | AccountID  | Data | Message |
+      | X123       | null | Account deleted successfully |
+      | Y456       | null | Account deleted successfully |
+
+Scenario Outline: A user can deposit to an account with different account numbers and deposit amounts
+    Given the user has an account with AccountNumber '<AccountNumber>
+    And verify the response data '<AccountNumber>', '<NewBalance>' and <Message>
+      
+    Examples:
+      | AccountNumber | Amount | NewBalance | Message                                       |
+      | X123           | 1000   | 2000       | 1000$ deposited to Account X123 successfully |
+      | Y456           | 500    | 1500       | 500$ deposited to Account Y456 successfully  |
+Feature: User can withdraw from an account
+
+  Scenario Outline: A user can withdraw from an account with different account numbers and withdrawal amounts
+    Given the user has an account with details '<AccountNumber>' and '<Amount>'
+    And on verifiication of the response data '<AccountNumber>', '<NewBalance>' and <Message>
+
+    Examples:
+      | AccountNumber | Amount | NewBalance | Message                                       |
+      | X123           | 1000   | 0          | 1000$ withdrawn from Account X123 successfully |
+      | Y456           | 500    | 500        | 500$ withdrawn from Account Y456 successfully  |
---
+++ BankAPITestsSpecflow/Steps/BankingScenariosSteps.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using Newtonsoft.Json;
+using RestSharp;
+using TechTalk.SpecFlow;
+using Xunit;
+
+[Binding]
+public class BankingScenariosSteps
+{
+    private RestClient client;
+    private IRestResponse response;
+
+    [Given(@"the user sends the request to create account with details '(.*)','(.*)', and '(.*)'")]
+    public void GivenTheUserSendsRequestToCreateAccount(string initialBalance, string accountName, string address)
+    {
+        var initialBalance = accountDetails["initialBalance"];
+        var accountName = accountDetails["accountName"];
+        var address = accountDetails["address"];
+
+        var requestPayload = new
+        {
+            InitialBalance = Convert.ToInt32(initialBalance),
+            AccountName = accountName,
+            Address = address
+        };
+
+        client = new RestClient("https://www.localhost:8080/api/account/create");
+        var request = new RestRequest(Method.POST);
+        request.AddJsonBody(JsonConvert.SerializeObject(requestPayload));
+        response = client.Execute(request);
+    }
+   [Then(@"I verify the status code '(.*)'")]
+   public void ThenIVerifyTheStatusCode(string expectedStatusCode)
+    {
+        if(response.statusCode == expectedStatusCode){
+            Console.WriteLine("API Successful!!");
+        }
+        else{
+            Console.WriteLine("Status code not matched");
+        }
+    }
+
+    [And(@"verify the response data '(.*)','(.*)','(.*)' and '(.*)'")]
+    public void AndVerifyTheResponseData(string expectedInitialBalance, string expectedAccountName, string expectedAccountNumber, string expectedMessage)
+    {
+        var InitialBalance = expectedResponse["expectedInitialBalance"];
+        var AccountName = expectedResponse["expectedAccountName"];
+        var AccountNumber = expectedResponse["expectedAccountNumber"];
+        var Message = expectedResponse["expectedMessage"];
+
+        var responseData = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        Assert.Equal(InitialBalance, responseData.Data.expectedInitialBalance.ToString());
+        Assert.Equal(AccountName, responseData.Data.expectedAccountName.ToString());
+        Assert.Equal(AccountNumber, responseData.Data.expectedAccountNumber.ToString());
+        Assert.Equal(Message, responseData.expectedMessage.ToString());
+        Assert.Empty(responseData.Errors);
+    }
+    [Given(@"the user has an account with AccountID ""(.*)""")]
+    public void GivenTheUserHasAnAccountWithAccountID(string accountID)
+    {
+        client = new RestClient($"https://www.localhost:8080/api/account/delete/{accountID}");
+        var request = new RestRequest(Method.DELETE);
+        response = client.Execute(request);
+    }
+
+    [Then(@"the response should contain Data: ""(.*)"" and Message: ""(.*)""")]
+    public void ThenTheResponseShouldContainDataAndMessage(string expectedData, string expectedMessage)
+    {
+        var data = expectedResponse["Data"];
+        var message = expectedResponse["Message"];
+
+        var responseData = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        Assert.Equal(Data, responseData.expectedData.ToString());
+        Assert.Equal(Message, responseData.expectedMessage.ToString());
+        Assert.Empty(responseData.Errors);
+    }
+    [Given(@"the user has an account with AccountNumber ""(.*)""")]
+    public void GivenTheUserHasAnAccountWithAccountNumber(string accountNumber)
+    {
+        // Additional setup logic can be added if needed
+        client = new RestClient($"https://www.localhost:8080/api/account/deposit");
+        var depositDetails = table.Rows[0];
+        var accountNumber = depositDetails["AccountNumber"];
+        var amount = depositDetails["Amount"];
+
+        var requestPayload = new
+        {
+            AccountNumber = accountNumber,
+            Amount = Convert.ToInt32(amount)
+        };
+
+        var request = new RestRequest(Method.PUT);
+        request.AddJsonBody(JsonConvert.SerializeObject(requestPayload));
+        response = client.Execute(request);
+    }
+
+    [Then(@"the response should contain AccountNumber: ""(.*)"", NewBalance: ""(.*)"", and Message: ""(.*)""")]
+    public void ThenTheResponseShouldContainAccountNumberNewBalanceAndMessage(string expectedAccountNumber, string expectedNewBalance, string expectedMessage)
+{
+    var responseData = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+    // Verify the response data
+    Assert.Equal(expectedAccountNumber, responseData.Data.AccountNumber.ToString());
+    Assert.Equal(expectedNewBalance, responseData.Data.NewBalance.ToString());
+    Assert.Equal(expectedMessage, responseData.Message.ToString());
+    Assert.Empty(responseData.Errors);
+
+}
+    [Given(@"the user has an account with details '(.*)' and '(.*)'")]
+    public void GivenTheUserHasAnAccountWithDetails(string accountNumber, int amount)
+    {
+        // Additional setup logic can be added if needed
+        client = new RestClient($"https://www.localhost:8080/api/account/withdraw");
+        var accountNumber = withdrawalDetails["AccountNumber"];
+        var amount = withdrawalDetails["Amount"];
+
+        var requestPayload = new
+        {
+            AccountNumber = accountNumber,
+            Amount = Convert.ToInt32(amount)
+        };
+
+        var request = new RestRequest(Method.PUT);
+        request.AddJsonBody(JsonConvert.SerializeObject(requestPayload));
+        response = client.Execute(request);
+    }
+
+    [Then(@"the response should contain AccountNumber: ""(.*)"", NewBalance: ""(.*)"", and Message: ""(.*)""")]
+    public void ThenTheResponseShouldContainAccountNumberNewBalanceAndMessage(string expectedAccountNumber, int expectedNewBalance, string expectedMessage)
+    {
+       var accountNumber = expectedResponse["AccountNumber"];
+        var newBalance = expectedResponse["NewBalance"];
+        var message = expectedResponse["Message"];
+
+        var responseData = JsonConvert.DeserializeObject<dynamic>(response.Content);
+
+        Assert.Equal(accountNumber, responseData.Data.AccountID.ToString());
+        Assert.Equal(newBalance, responseData.Data.NewBalance.ToString());
+        Assert.Equal(message, responseData.Message.ToString());
+        Assert.Empty(responseData.Errors);
+    }
+}
